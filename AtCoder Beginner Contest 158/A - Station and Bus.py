498a3b614f8012ea4de6e87bd50ab5da416eb9e0
kimhakheng <45730507+kimhakheng@users.noreply.github.com>
2020-08-14 12:04:50 +0700

    Update A - Station and Bus.py

--- AtCoder Beginner Contest 158/A - Station and Bus.py
+++ AtCoder Beginner Contest 158/A - Station and Bus.py
@@ -1,11 +1,11 @@
-    S = input()
-    n = len(S)
-    if n == 3:
-      a = S.split()
-      x = S[0]
-      y = S[1]
-      z = S[2]
-      if x != y or y != z:
-        print("Yes")
-      else:
-        print("No")
+S = input()
+n = len(S)
+if n == 3:
+  a = S.split()
+  x = S[0]
+  y = S[1]
+  z = S[2]
+  if x != y or y != z:
+    print("Yes")
+  else:
+    print("No")
