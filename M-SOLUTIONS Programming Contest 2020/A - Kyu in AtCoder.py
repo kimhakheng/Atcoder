e2373b978b3b8c432eb03a57f12beed6076faf8a
kimhakheng <45730507+kimhakheng@users.noreply.github.com>
2020-08-14 12:56:45 +0700

    Create A - Kyu in AtCoder.py

---
+++ M-SOLUTIONS Programming Contest 2020/A - Kyu in AtCoder.py
@@ -0,0 +1,17 @@
+n = int(input())
+if n >= 400 and n < 600:
+  print(8)
+elif n >= 600 and n < 800:
+  print(7)
+elif n >= 800 and n < 1000:
+  print(6)
+elif n >= 1000 and n < 1200:
+  print(5)
+elif n >= 1200 and n < 1400:
+  print(4)
+elif n >= 1400 and n < 1600:
+  print(3)
+elif n >= 1600 and n < 1800:
+  print(2)
+elif n >= 1800 and n < 2000:
+  print(1)
